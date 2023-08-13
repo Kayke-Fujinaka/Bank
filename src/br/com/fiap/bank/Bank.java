d0b4dcbf4adcc12a6e05f9e73430182cb9ef6286
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-08-13 09:15:13 -0300

    feat: add validation if deposit amount is invalid

--- src/br/com/fiap/bank/Bank.java
+++ src/br/com/fiap/bank/Bank.java
@@ -5,14 +5,25 @@ public class Bank {
 	int agency;
 	double balance;
 
+	private static final String INVALID_DEPOSIT_MESSAGE = "Insira um valor positivo para depósito.";
+
 	public Bank(int number, int agency, double balance) {
 		this.setNumber(number);
 		this.setAgency(agency);
 		this.setBalance(balance);
 	}
 
-	public void deposit(double value) {
-		this.balance += value;
+	public void deposit(double amount) {
+		if (!isValidDepositAmount(amount)) {
+			System.out.println(INVALID_DEPOSIT_MESSAGE);
+			return;
+		}
+
+		this.balance += amount;
+	}
+
+	private boolean isValidDepositAmount(double amount) {
+		return amount >= 0;
 	}
 
 	public void withdraw(double value) {
--- src/br/com/fiap/bank/Test.java
+++ src/br/com/fiap/bank/Test.java
@@ -2,19 +2,19 @@ package br.com.fiap.bank;
 
 public class Test {
 	public static void main(String[] args) {
-		Bank cc = new Bank();
+		Bank cc = new Bank(0, 0, 0);
 		cc.balance = 1000;
 		cc.agency = 123;
 		cc.number = 321;
-		
+
 		cc.deposit(100);
-		
+
 		System.out.println(cc.getBalance());
-		
+
 		Bank savings = new Bank(100, 123, 1000);
-		
+
 		savings.withdraw(50);
-		
+
 		System.out.println(savings.getBalance());
-    }
+	}
 }
