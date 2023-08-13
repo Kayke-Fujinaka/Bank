42f47750797a6d9a188cab6ba5c8b9d406bf77fc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-08-13 09:04:32 -0300

    refactor: format code

--- src/br/com/fiap/bank/Bank.java
+++ src/br/com/fiap/bank/Bank.java
@@ -5,42 +5,43 @@ public class Bank {
 	int agency;
 	double balance;
 
-	public Bank(){}
+	public Bank() {
+	}
 
-	public Bank(int number, int agency, double balance){
+	public Bank(int number, int agency, double balance) {
 		this.setNumber(number);
 		this.setAgency(agency);
 		this.setBalance(balance);
 	}
 
-	public void deposit(double value){
+	public void deposit(double value) {
 		this.balance += value;
 	}
 
-	public void withdraw(double value){
+	public void withdraw(double value) {
 		this.balance -= value;
 	}
 
 	public int getNumber() {
 		return number;
 	}
-	
+
 	public void setNumber(int number) {
 		this.number = number;
 	}
-	
+
 	public int getAgency() {
 		return agency;
 	}
-	
+
 	public void setAgency(int agency) {
 		this.agency = agency;
 	}
-	
+
 	public double getBalance() {
 		return balance;
 	}
-	
+
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
